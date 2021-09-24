e86f7fb8519c2b1d11c8482851350d7f4c383ee8
Praveen Kumar S <55323914+Stonekeeper@users.noreply.github.com>
2021-09-24 16:14:58 +0000

    Add files via upload

---
+++ game.js
@@ -0,0 +1,88 @@
+var level = 0;
+
+var started = false;
+var userClickedPattern = [];
+
+var gamePattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence() {
+  userClickedPattern=[];
+  level++;
+  $("h1").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 3 + 1);
+
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColor);
+
+}
+
+$(".btn").click(function() {
+  var userChosenColor = this.id;
+  userClickedPattern.push(userChosenColor);
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("." + currentColor).addClass("pressed");
+  setTimeout(function() {
+    $("." + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+
+$(document).keypress(function() {
+  if (!started) {
+    $("h1").text("Level " + level);
+    nextSequence();
+    started = true;
+
+  }
+});
+
+
+function checkAnswer(currentLevel)
+{
+  if(gamePattern[currentLevel]===userClickedPattern[currentLevel])
+  {
+    console.log("Success");
+
+    if(userClickedPattern.length===gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      },1000);
+    }
+  }
+  else
+  {
+    console.log("Wrong");
+    $("h1").text("Game Over, Press any key to restart");
+    var wrong=new Audio("sounds/wrong.mp3");
+    wrong.play();
+    $("body").addClass("game-over");
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver();
+  }
+}
+
+function startOver()
+{
+  level=0;
+  gamePattern=[];
+  started=false;
+}
